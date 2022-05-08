28d4c842fe22a260f8312636a7f4c3cccc1d9452
Philip Garrison <absently.ricotta645@anonaddy.me>
2022-05-08 15:07:59 -0700

    Add home-manager setup

--- flake.nix
+++ flake.nix
@@ -4,15 +4,22 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    home.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
  
-  outputs = { self, nixpkgs, home, ... }: {
+  outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.Niflheimr = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./hosts/niflheimr/default.nix
         ./modules/shell/alias.nix
+        home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.philip = import ./users/philip.nix;
+          }
        ];
     };
   };
