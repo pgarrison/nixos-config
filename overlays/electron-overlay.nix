ff03f376f1d9e691ac30a6789afcbb4db523e174
Philip Garrison <absently.ricotta645@anonaddy.me>
2022-05-11 18:30:52 -0700

    ozone: working vscodium and chromium display

--- overlays/electron-overlay.nix
+++ overlays/electron-overlay.nix
@@ -6,24 +6,24 @@ let
       postFixup = (old.postFixup or "") + ''
         wrapProgram $out/bin/${bin} \
           --add-flags "--enable-features=UseOzonePlatform" \
-          --add-flags "--ozone-platform=wayland"
+          --add-flags "--ozone-platform=wayland" \
+          --add-flags "--log debug" \
+          --add-flags "--log debug"
       '';
+      # The --log debug flags are just an ugly workaround for https://github.com/microsoft/vscode/issues/146349
     }
   );
 in
 super:
   {
-    signal-desktop = enableWayland super.signal-desktop "signal-desktop";
+    #signal-desktop = enableWayland super.signal-desktop "signal-desktop";
     #slack = enableWayland super.slack "slack";
     #discord = enableWayland super.discord "discord";
     #vscode = enableWayland super.vscode "code";
-    #vscodium = enableWayland super.vscodium "vscodium";
-    chromium = enableWayland super.chromium "chromium";
-    /*
-    google-chrome-beta = (
-      super.google-chrome-beta.override {
+    vscodium = enableWayland super.vscodium "codium";
+    chromium = (
+      super.chromium.override {
         commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
       }
     );
-    */
   }
