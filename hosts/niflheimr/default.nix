2c53cb25a410ecd6161304045ce61de176afe8c1
Philip Garrison <absently.ricotta645@anonaddy.me>
2022-05-11 17:29:33 -0700

    electron: attempt to call wrapPackage (broken)

--- hosts/niflheimr/default.nix
+++ hosts/niflheimr/default.nix
@@ -87,6 +87,10 @@
 
   nixpkgs.config.allowUnfree = true;
 
+  nixpkgs.overlays = [
+    (import ../../overlays/electron-overlay.nix)
+  ];
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
