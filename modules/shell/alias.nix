44b61da20ee2454e1422b7094972b366ced473ca
Philip Garrison <absently.ricotta645@anonaddy.me>
2022-05-11 23:53:35 -0700

    alias: update nixos

--- modules/shell/alias.nix
+++ modules/shell/alias.nix
@@ -1,8 +1,9 @@
 { home, ... }:
 { 
   home.shellAliases = {
-    rebuild = "nixos-rebuild switch --flake /home/philip/nixos-config --use-remote-sudo";
     kexp = "mpv http://live-mp3-128.kexp.org";
+    rebuild = "nixos-rebuild switch --flake /home/philip/nixos-config --use-remote-sudo";
+    update = "nix flake update ~/nixos-config";
     nixos-list-updates = ''ls -v /nix/var/nix/profiles | tail -n 2 | awk '{print "/nix/var/nix/profiles/" $0}' - | xargs nvd diff'';
   };
 }
