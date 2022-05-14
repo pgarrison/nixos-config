cb2972708fe2732f9dd253a339beaddf8e19455f
Philip Garrison <absently.ricotta645@anonaddy.me>
2022-05-13 18:17:18 -0700

    git: define 'git rs'

--- modules/git.nix
+++ modules/git.nix
@@ -13,16 +13,18 @@ in
   };
 
   config = mkIf cfg.enable {
-  programs.git = {
-    enable = true;
-    aliases = {
-      lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      cm = "commit -m";
-      ap = "add -p";
-      dc = "diff --cached";
-      ds = "diff --stat";
-      dcs = "diff --cached --stat";
+    programs.git = {
+      enable = true;
+      aliases = {
+        lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+        cm = "commit -m";
+        ap = "add -p";
+        dc = "diff --cached";
+        ds = "diff --stat";
+        dcs = "diff --cached --stat";
+        rs = "restore --staged .";
+      };
+      userName = "Philip Garrison";
     };
-    userName = "Philip Garrison";
   };
 }
