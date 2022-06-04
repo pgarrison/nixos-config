9859c147020fb7cfea5f1971d85823e368838ec2
Philip Garrison <absently.ricotta645@anonaddy.me>
2022-06-04 03:56:08 -0700

    gtk3: no luck
    Tried using the instructions here:
      https://www.reddit.com/r/NixOS/comments/qc6jkl/sway_gtk_themes_wont_set/

--- modules/sway.nix
+++ modules/sway.nix
@@ -55,13 +55,29 @@ in
       ];
     };
 
-    wayland.windowManager.sway = {
+    wayland.windowManager.sway = let
+      gsettings = "${pkgs.glib}/bin/gsettings";
+      gnomeSchema = "org.gnome.desktop.interface";
+      importGsettings = pkgs.writeShellScript "import_gsettings.sh" ''
+        config="/home/alternateved/.config/gtk-3.0/settings.ini"
+        if [ ! -f "$config" ]; then exit 1; fi
+        gtk_theme="$(grep 'gtk-theme-name' "$config" | sed 's/.*\s*=\s*//')"
+        icon_theme="$(grep 'gtk-icon-theme-name' "$config" | sed 's/.*\s*=\s*//')"
+        cursor_theme="$(grep 'gtk-cursor-theme-name' "$config" | sed 's/.*\s*=\s*//')"
+        font_name="$(grep 'gtk-font-name' "$config" | sed 's/.*\s*=\s*//')"
+        ${gsettings} set ${gnomeSchema} gtk-theme "$gtk_theme"
+        ${gsettings} set ${gnomeSchema} icon-theme "$icon_theme"
+        ${gsettings} set ${gnomeSchema} cursor-theme "$cursor_theme"
+        ${gsettings} set ${gnomeSchema} font-name "$font_name"
+      '';
+    in {
       enable = true;
       #package = pkgs.sway-unwrapped;
       wrapperFeatures = { base = true; gtk = true; };
       #xwayland = cfg.xwayland;
       config = rec {
         startup = [
+          { command = "${importGsettings}"; }
           {
             command = "wlr-randr --output eDP-1 --scale 1.5";
             always = true;
