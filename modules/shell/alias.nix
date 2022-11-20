d045626d649251e2c2b0206d01dbb40e43feef4d
Philip Garrison <absently.ricotta645@anonaddy.me>
2022-11-20 14:28:49 -0800

    wifi command

--- modules/shell/alias.nix
+++ modules/shell/alias.nix
@@ -2,8 +2,9 @@
 { 
   home.shellAliases = {
     kexp = "mpv http://live-mp3-128.kexp.org";
+    nixos-list-updates = ''ls -v /nix/var/nix/profiles | tail -n 2 | awk '{print "/nix/var/nix/profiles/" $0}' - | xargs nvd diff'';
     rebuild = "nixos-rebuild switch --flake /home/philip/nixos-config --use-remote-sudo";
     update = "nix flake update ~/nixos-config";
-    nixos-list-updates = ''ls -v /nix/var/nix/profiles | tail -n 2 | awk '{print "/nix/var/nix/profiles/" $0}' - | xargs nvd diff'';
+    wifi = "nmcli device wifi";
   };
 }
