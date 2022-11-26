b6f2122e7eeb4e840fe38e9713160ccda7a1e9db
Philip Garrison <absently.ricotta645@anonaddy.me>
2022-11-26 15:41:34 -0800

    weather, moon, shell aliases

--- modules/shell/alias.nix
+++ modules/shell/alias.nix
@@ -2,9 +2,12 @@
 { 
   home.shellAliases = {
     kexp = "mpv http://live-mp3-128.kexp.org";
+    moon = "curl 'wttr.in/moon?Seattle+US'";
     nixos-list-updates = ''ls -v /nix/var/nix/profiles | tail -n 2 | awk '{print "/nix/var/nix/profiles/" $0}' - | xargs nvd diff'';
     rebuild = "nixos-rebuild switch --flake /home/philip/nixos-config --use-remote-sudo";
+    shell = "nix-shell --run zsh -p";
     update = "nix flake update ~/nixos-config";
+    weather = "curl 'wttr.in/?2nq'";
     wifi = "nmcli device wifi";
   };
 }
