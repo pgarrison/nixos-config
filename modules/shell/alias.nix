06df8c236edc40160a307a515f6fcd4b63b09593
Philip Garrison <absently.ricotta645@anonaddy.me>
2023-03-21 21:28:05 -0700

    tree --gitignore

--- modules/shell/alias.nix
+++ modules/shell/alias.nix
@@ -9,6 +9,7 @@
     npr = "fun(){ if [ -z \"\$1\" ]; then curl -s https://text.npr.org | htmlq a.topic-title --text | nl -w 2; else curl -s \$(curl -s https://text.npr.org/ | htmlq a.topic-title -b https://text.npr.org -a href | head -n \$1 | tail -1) | grep -v 'Related Story' | htmlq article -r .slug-line --text | grep -v '^ *$' | less; fi; unset -f fun }; fun";
     rebuild = "nixos-rebuild switch --flake /home/philip/nixos-config --use-remote-sudo";
     shell = "nix-shell --run zsh";
+    tree = "tree --gitignore";
     update = "nix flake update ~/nixos-config";
     weather = "curl 'wttr.in/?2nq'";
     wifi = "nmcli device wifi";
