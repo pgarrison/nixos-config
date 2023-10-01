a2b0ca1c7b528b5c14e939cf9d23aac6a53975a5
Philip Garrison <absently.ricotta645@anonaddy.me>
2023-09-30 22:22:57 -0700

    nixos-clean-old-generations

--- modules/shell/alias.nix
+++ modules/shell/alias.nix
@@ -5,6 +5,7 @@
     kexp = "mpv http://live-mp3-128.kexp.org";
     moon = "curl 'wttr.in/moon?Seattle+US'";
     nixos-list-updates = ''ls -v /nix/var/nix/profiles | tail -n 2 | awk '{print "/nix/var/nix/profiles/" $0}' - | xargs nvd diff'';
+    nixos-clean-old-generations = "sudo nix profile wipe-history --profile /nix/var/nix/profiles/system --older-than 365d";
     # Depends on htmlq. Usage: npr [n]
     npr = "fun(){ if [ -z \"\$1\" ]; then curl -s https://text.npr.org | htmlq a.topic-title --text | nl -w 2; else curl -s \$(curl -s https://text.npr.org/ | htmlq a.topic-title -b https://text.npr.org -a href | head -n \$1 | tail -1) | grep -v 'Related Story' | htmlq article -r .slug-line --text | grep -v '^ *$' | less; fi; unset -f fun }; fun";
     rebuild = "nixos-rebuild switch --flake /home/philip/nixos-config --use-remote-sudo";
